dae7ea19606a06170ec58979a80d7208e84c036e
stefan-scott <scottst@spsd.sk.ca>
2025-02-24 11:35:47 -0600

    Update sketch.js

--- 05 Drawing with Single Loops/sketch.js
+++ 05 Drawing with Single Loops/sketch.js
@@ -1,9 +1,6 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Drawing with Single Loops
+// Mr. Scott
+// Feb 24th
 
 
 function setup() {
@@ -12,4 +9,20 @@ function setup() {
 
 function draw() {
   background(220);
+  circleLine();
+}
+
+function circleLine(){
+  //use a loop (for or while) to draw a line
+  //of circles side by side.
+  let d = 40; //diameter of each circle
+  let y = height/2;
+  let xStart = 0;
+  let xEnd = mouseX;   //width*0.75;
+
+  //use a loop to do the drawing
+  for(let x = xStart; x <= xEnd; x+=d){
+    //x: 0  40  80  120  160   200  240
+    circle(x, y, d);
+  }
 }
