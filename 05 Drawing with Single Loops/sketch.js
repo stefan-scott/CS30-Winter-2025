02287f6c64defcab6a786aed114b736adca57bf3
stefan-scott <scottst@spsd.sk.ca>
2025-02-24 12:03:39 -0600

    finished gradient demo

--- 05 Drawing with Single Loops/sketch.js
+++ 05 Drawing with Single Loops/sketch.js
@@ -8,19 +8,37 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  gradientBackground();
   circleLine();
+
+  //screen gets updated here.
+}
+
+function gradientBackground(){
+  //create a gradient to use as background
+  let h = 10;
+
+  //use a loop to draw vertical stack of rectangles
+  for(let y = 0; y < height; y += h){
+    noStroke();
+    let mappedY = map(y,0,height,0,255);
+    let reversedY = map(y,0,height,255,0);
+    fill(mappedY, reversedY, mouseX/3);
+    rect(0, y, width, h);
+  }
+
 }
 
 function circleLine(){
   //use a loop (for or while) to draw a line
   //of circles side by side.
-  let d = 40; //diameter of each circle
+  let d = 30; //diameter of each circle
   let y = height/2;
   let xStart = 0;
-  let xEnd = mouseX;   //width*0.75;
+  let xEnd = width;
 
   //use a loop to do the drawing
+  //RESULTS IN A SINGLE IMAGE. NO ANIMATION!!!
   for(let x = xStart; x <= xEnd; x+=d){
     //x: 0  40  80  120  160   200  240
     circle(x, y, d);
