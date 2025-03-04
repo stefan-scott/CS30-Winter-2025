ee537849b8f28a64bc45fd6c246eca9cafd09eb9
stefan-scott <scottst@spsd.sk.ca>
2025-03-04 11:38:13 -0600

    Update sketch.js

--- 09 Terrain Generation Starter/sketch.js
+++ 09 Terrain Generation Starter/sketch.js
@@ -3,11 +3,38 @@
 // Mr. Scott
 // March 3, 2025
 
+let rectWidth = 1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  generateTerrain();
+}
+
+function generateTerrain(){
+  // use a loop to generate and draw
+  // several rectangles side to side
+  // to look like some 2D terrain
+  rectMode(CORNERS);
+
+  for(let x = 0; x < width; x += rectWidth){
+    //generate a random height. 
+    //change this from using random() to noise()
+    let rectHeight = random(50,500); 
+    
+    //calculate the other corner of our rectangle
+    let x2 = x + rectWidth;
+    let y2 = height - rectHeight;
+
+    rect(x, height, x2, y2);
+  }
+  
+  rectMode(CORNER);
 }
 
 function draw() {
-  background(220);
+  // background(220);
+  // generateTerrain();
 }
+
+
+
