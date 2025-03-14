8be5d8d9a4fe48b80f77dc1b4ea46e15d036436b
stefan-scott <scottst@spsd.sk.ca>
2025-03-14 11:11:00 -0600

    Update sketch.js

--- 13 Objects Demo (Books)/sketch.js
+++ 13 Objects Demo (Books)/sketch.js
@@ -2,13 +2,35 @@
 // Mr. Scott
 // March 12, 2025
 
+// 1. Add two more book objects (each in a var)
+//    and add to the shelf... (connected to my)
+//    book.
+
+// 2. Use a loop and an array to generate 20+ 
+//    skinny books
+//    (can be all named the same if needed)
+
 let myBook;
+let bookshelf = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+ //Make 20 books in a row
+ let x = 50;
+ let covers = ["softcover","hardcover","leatherbound"];
+ for(let i = 0; i < 20; i++){
+   let choice = int(random(3));  //0, 1, 2
+  
+   bookshelf.push(new Book("A", "Mr. Booth",
+   1111111111, covers[choice], 200, x));
+   x += 20;
+ }
+ 
   myBook = new Book("CS30 Text", "Mr. Scott",
     1234567891011, "leatherbound",
     515, width * 0.3);
+
+
   myBook.printOut();
 }
 
