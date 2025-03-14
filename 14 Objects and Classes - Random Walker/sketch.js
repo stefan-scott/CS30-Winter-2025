e007114069b4d97f38a647a501de042e0b76fd8b
stefan-scott <scottst@spsd.sk.ca>
2025-03-14 12:16:24 -0600

    done

--- 14 Objects and Classes - Random Walker/sketch.js
+++ 14 Objects and Classes - Random Walker/sketch.js
@@ -4,16 +4,52 @@
 // a first look at working with multiple objects
 
 let singleWalker;
+let walkers = [];
+const NUM_WALKERS = 15000;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   singleWalker = new Walker(100, 150, "green");
+  initWalkers();
+  noStroke();
+}
+
+function initWalkers(){
+  //create a bunch of walker objects, put in array
+  for(let i = 0; i < NUM_WALKERS; i++){
+    let c = color(random(255),random(255),random(255));
+    let w = new Walker(random(width), random(height),c);
+    walkers.push(w);
+  }
 }
 
 function draw() {
   background(220);
-  singleWalker.move();
-  singleWalker.display();
+  // singleWalker.move();
+  // singleWalker.display();
+
+  // for(let currentWalker of walkers){ //loop by item
+  //   //but doesn't let us remove any objects.
+  //   currentWalker.move();
+  //   currentWalker.display();
+  // }
+  fill(100,50,255,50);
+  circle(mouseX, mouseY, 60);
+
+  for(let i = 0; i < walkers.length; i++){ //loop by index
+    //this allows us to delete
+    let w = walkers[i];
+    w.move();
+    w.display();
+
+    //Ask if the current object is close to the mouse
+    if(dist(w.x, w.y, mouseX, mouseY) < 30){
+      //to delete from an arbitrary point in array: splice()
+      walkers.splice(i,1);
+    }
+  }
+
+
 }
 
 class Walker {
@@ -28,7 +64,7 @@ class Walker {
   display(){  //render the walker on screen
     rectMode(CENTER);
     fill(this.c);
-    square(this.x, this.y, this.size);
+    circle(this.x, this.y, this.size);
   }
 
   move(){
