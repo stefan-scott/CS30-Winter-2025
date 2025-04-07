f8e14f563e0cbf154d9db448c06357cc5927cb02
stefan-scott <scottst@spsd.sk.ca>
2025-04-07 11:45:53 -0600

    in progress

--- 17 2D Array Basics/sketch.js
+++ 17 2D Array Basics/sketch.js
@@ -2,11 +2,11 @@
 // Mr. Scott
 // April 3, 2025
 // Working with 2D Arrays, Visualizations
-
-let grid =
-  [[0, 60, 120, 180, 240],
-  [240, 180, 120, 60, 0],
-  [0, 200, 0, 200, 0]
+//0   255
+let grid = [
+  [0,  0,   255, 255, 0],
+  [255, 255, 0,   255, 0],
+  [0,   0,   0,   255, 0]
   ];
 
 let squareSize = 60;
@@ -40,19 +40,32 @@ function getCurrentX() {
   return floor(constrainedX / squareSize);
 }
 
-function checkForMouse() {
+function mousePressed() {
   //flip current tile to a random greyscale value
-  if (mouseIsPressed) {
-    let x = getCurrentX();
-    let y = getCurrentY();
-    grid[y][x] = floor(random(255));
-  }
+  //only do something if mouseX/mouseY are on the canvas...
+  
+  let x = getCurrentX();
+  let y = getCurrentY();
+  
+  //always: flip the "current" tile
+  flip(x,y);
 
+  //sometimes: (depending on position) flip the neighbours
+  if(y > 0) flip(x, y-1);  //NORTH 
+  if(x > 0) flip(x-1, y);  //WEST
+  if(x < NUM_COLS-1) flip(x+1, y); //EAST
+  if(y < NUM_ROWS-1) flip(x, y+1); //SOUTH
+  
 }
+
+function flip(x, y){
+  //take a tile and invert its value
+  if (grid[y][x] === 0) grid[y][x] = 255;
+  else grid[y][x] = 0;
+}
+
 
 function draw() {
   background(220);
   renderGrid();
-  checkForMouse();
-
 }
